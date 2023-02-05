4d5072a8c02c51ff992ab074b3d639684c79c60e
Niels van Dam <nielsvdam@hotmail.com>
2023-02-05 03:01:59 +0100

    random rotation

--- Assets/Scripts/Grid/RockTile.cs
+++ Assets/Scripts/Grid/RockTile.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class RockTile : MonoBehaviour
 {
     public List<GameObject> rocks;
-    
+    [SerializeField] private float[] possibleRotations;
     private void Start()
     {
-        Instantiate(rocks[Random.Range(0, rocks.Count)], transform);
+        Instantiate(rocks[Random.Range(0, rocks.Count)], transform.position, Quaternion.Euler(0,0,possibleRotations[Random.Range(0, possibleRotations.Length)]), transform);
     }
 }
